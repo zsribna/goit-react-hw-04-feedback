0a9aaaf89910f4f68d2bcdb06fd8025cb8c2e50f
zsribna <zsribna@gmail.com>
2023-10-24 21:04:03 +0300

    add

--- src/components/Notification/Notification.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { Text } from './Notification.styled'
-export const Notification = ({message}) => {
-  return  <Text >{ message}</Text>
-  
-}
--- src/components/Notification/Notification.styled.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-export const Text = styled.p`
-  
-    padding-left: ${p => p.theme.spacing(4)};
-
-`;
